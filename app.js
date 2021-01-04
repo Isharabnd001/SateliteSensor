9bac4ff30b6416fe2eb2327b49ec4b5e0916eca1
Ishara <Ishara>
2021-01-04 12:28:43 +0530

    change

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var app = express();
 var date = new Date;
 // Routes
 app.get('/', function(req, res) {
-  res.send({"health":"OK",
+  res.send({"health":"FAILED",
 "location":new Date().getMinutes()*(new Date().getHours()+1)
 });
 });
 
 // Listen
-var port = process.env.PORT || 3010;
+var port = process.env.PORT || 3000;
 app.listen(port);
 console.log('Listening on localhost:'+ port);
